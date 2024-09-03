a5ed1ffe73d60c69141f5ff38355cb8d6cd49519
Abdulkadir <119424677+Abdulkadir1614@users.noreply.github.com>
2024-09-03 10:06:51 +0800

    done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hello");
+
+const grade = 90 ;
+
+if(grade >= 90 || grade === 100){
+    console.log("You got an A grade");
+}
+else if(grade >= 75 || grade === 90){
+    console.log("You got an B grade");
+}
+else if(grade >= 60 || grade === 75){
+    console.log("You got an C grade");
+}
+else if(grade < 60){
+    console.log("You got an Failed ");
+}
+else {
+    console.log("You grade is invalid");
+}
